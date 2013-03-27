38d5d8dbec27f92b9968f3ef9eca6215c4fa04a7
tokuhirom <tokuhirom@gmail.com>
2013-03-27 19:18:59 +0900

    MFPM.pm

---
+++ lib/MFPM.pm
@@ -0,0 +1,34 @@
+package MFPM;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=head1 NAME
+
+MFPM - It's new $module
+
+=head1 SYNOPSIS
+
+    use MFPM;
+
+=head1 DESCRIPTION
+
+MFPM is ...
+
+=head1 LICENSE
+
+Copyright (C) tokuhirom
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tokuhirom E<lt>tokuhirom@gmail.comE<gt>
+
